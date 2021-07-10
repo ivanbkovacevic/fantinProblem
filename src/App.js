0f1ea14fdb9176dbb49c4f0d67970566262f32d6
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2021-07-10 22:42:19 +0200

    dcsvs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <h1>Evo Fanta</h1>
+      Selektuj ime(polisu)
+      <p>Pa onda chekiraj Features</p>
+      <p>
+        Provalices da se menja state za svaku features za selectovane polise
+      </p>
       <Policy />
     </div>
   );
